360c23b4c263f01a09e6790949e6f8e00462c095
Linazzy <36626979+Linazzy@users.noreply.github.com>
2018-05-28 10:38:34 +0200

    add validador

--- src/modelo/Almacen.java
+++ src/modelo/Almacen.java
@@ -1,7 +1,20 @@
 package modelo;
 
-public class Almacen {
+import java.util.TreeMap;
+
+public class Almacen <T,K>{
 
 	//TreeMap Cliente
 	//DataNoseque pedido
+	private String pathIndiceDni;
+	private Cliente pathCliente;
+	private TreeMap<K, Integer> indice;
+	
+	public Almacen(String pathIndiceDni, Cliente pathCliente){
+		super();
+		this.pathIndiceDni= pathIndiceDni;
+		this.pathCliente= pathCliente;
+	}
+	
+	
 }
