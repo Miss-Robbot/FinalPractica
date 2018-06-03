00e6c73eba66c027110e211bb97fc0546d568c72
yolyle7 <36626947+yolyle7@users.noreply.github.com>
2018-06-04 00:46:45 +0200

    cosas de pedidos

--- src/controlador/Consultar.java
+++ src/controlador/Consultar.java
@@ -7,89 +7,113 @@ import javax.swing.JComboBox;
 import javax.swing.JTextArea;
 
 import modelo.Articulo;
-import modelo.Cliente;
 import modelo.DatoActual;
 import modelo.Linea;
 import modelo.Pedido;
 import modelo.Precio;
 
-public class Consultar extends Acciones{
+public class Consultar extends Acciones {
 
-	//Pueden modificarse tanto el void como la entrada de datos
-	
+	// Pueden modificarse tanto el void como la entrada de datos
+
 	/**
-	 * Pone en el textArea de informacion (en consultar) todos los precios antiguos de x artículo (controlar con el combobox en paraui)
+	 * Pone en el textArea de informacion (en consultar) todos los precios
+	 * antiguos de x artículo (controlar con el combobox en paraui)
+	 * 
 	 * @param articulo
 	 * @return
 	 */
-	public void consultarPreciosAntiguos(Articulo articulo, JTextArea txtInformacion){
-		LinkedList<Precio> preciosArticulo=getLogica().preciosArticulo(articulo);
-		preciosArticulo.pop(); //Tener cuidado que este quite el último añadido
-		String informacion="";
-		for (Iterator iterator = preciosArticulo.iterator(); iterator.hasNext();) {
+	public void consultarPreciosAntiguos(Articulo articulo,
+			JTextArea txtInformacion) {
+		LinkedList<Precio> preciosArticulo = getLogica()
+				.preciosArticulo(articulo);
+		preciosArticulo.pop(); // Tener cuidado que este quite el último añadido
+		String informacion = "";
+		for (Iterator iterator = preciosArticulo.iterator(); iterator
+				.hasNext();) {
 			Precio precio = (Precio) iterator.next();
-			informacion.concat(preciosArticulo.toString()+"\n");
+			informacion.concat(preciosArticulo.toString() + "\n");
 		}
 		txtInformacion.setText(informacion);
 	}
-	
+
 	/**
-	 * Pone en el textArea de informacion (en consultar) toda la información de todos los artículos de x pedido. Este pedido 
-	 * estará controlado en el paraui por el combobox
+	 * Pone en el textArea de informacion (en consultar) toda la información de
+	 * todos los artículos de x pedido. Este pedido estará controlado en el
+	 * paraui por el combobox
 	 */
-	public void consultarPedido(Pedido pedido, JTextArea txtInformacion){
-		LinkedList<Articulo> articulos= getLogica().articulosEnPedido(pedido);
-		String informacionArticulos="";
+	public void consultarPedido(Pedido pedido, JTextArea txtInformacion) {
+		LinkedList<Articulo> articulos = getLogica().articulosEnPedido(pedido);
+		String informacionArticulos = "";
 		for (Iterator iterator = articulos.iterator(); iterator.hasNext();) {
 			Articulo articulo = (Articulo) iterator.next();
-			informacionArticulos.concat(articulo.toString()+"\n");
+			informacionArticulos.concat(articulo.toString() + "\n");
 		}
 		txtInformacion.setText(informacionArticulos);
 	}
-	
+
 	/**
-	 * Pone en el textArea (enlazar con panelConsultar) toda la información del artículo
-	 * elegir el articulo en el paraui con el combobox
+	 * Pone en el textArea (enlazar con panelConsultar) toda la información del
+	 * artículo elegir el articulo en el paraui con el combobox
+	 * 
 	 * @param articulo
 	 * @param txtInformacion
 	 */
-	public void consultarArticulo(Articulo articulo, JTextArea txtInformacion){
+	public void consultarArticulo(Articulo articulo, JTextArea txtInformacion) {
 		txtInformacion.setText(articulo.toString());
 	}
-	
+
 	/**
 	 * Llena el combobox de artículos
+	 * 
 	 * @param combobox
 	 * @param articulos
 	 */
-	public void rellenarComboboxArticulo(JComboBox<Articulo> combobox){
-		LinkedList<Articulo> articulos= conseguirListaArticulos();
+	public void rellenarComboboxArticulo(JComboBox<Articulo> combobox) {
+		LinkedList<Articulo> articulos = conseguirListaArticulos();
 		for (Iterator iterator = articulos.iterator(); iterator.hasNext();) {
 			Articulo articulo = (Articulo) iterator.next();
 			combobox.addItem(articulo);
 		}
 	}
-	
-	public void rellenarComboboxPedido(JComboBox<Pedido> comboBox, DatoActual datoActual) {
-		//LinkedList<Pedido> pedidos = 
+
+	public void rellenarComboboxPedido(JComboBox<Pedido> comboBox,
+			DatoActual datoActual) {
+		LinkedList<Pedido> pedidos = conseguirListaPedidos();
+		for (Iterator iterator = pedidos.iterator(); iterator.hasNext();) {
+			Pedido pedido = (Pedido) iterator.next();
+			comboBox.addItem(pedido);
+		}
 	}
-	
+
 	/**
 	 * Tiene todos los artículos en una lista del pedido de dato actual
+	 * 
 	 * @param datoActual
 	 */
-	public LinkedList<Articulo> conseguirListaArticulos(){
-		LinkedList<Articulo> articulos=new LinkedList<Articulo>();
-		
-		
-		for (Iterator iterator = getLogica().getDatoActual().getPedidoActual().getLineas().iterator(); iterator.hasNext();) {
+	public LinkedList<Articulo> conseguirListaArticulos() {
+		LinkedList<Articulo> articulos = new LinkedList<Articulo>();
+
+		for (Iterator iterator = getLogica().getDatoActual().getPedidoActual()
+				.getLineas().iterator(); iterator.hasNext();) {
 			Linea linea = (Linea) iterator.next();
 			articulos.add(linea.getArticulo());
-			
+
 		}
 		return articulos;
 	}
-	public void consultarCliente(){
-		
+	public LinkedList<Pedido> conseguirListaPedidos() {
+		LinkedList<Pedido> pedidos = new LinkedList<Pedido>();
+
+		for (Iterator iterator = getLogica().getDatoActual().getClienteActual()
+				.getPedidos().iterator(); iterator.hasNext();) {
+			Pedido pedido = (Pedido) iterator.next();
+			pedidos.add(pedido);
+
+		}
+		return pedidos;
+	}
+	public void consultarCliente() {
+
 	}
 }
