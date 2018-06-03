2e7125d4299735c76d9432c73add260ffbea8f76
yolyle7 <36626947+yolyle7@users.noreply.github.com>
2018-06-03 18:48:39 +0200

    vistaInicio

--- src/vista/PanelInicio.java
+++ src/vista/PanelInicio.java
@@ -1,9 +1,32 @@
 package vista;
 
+import java.awt.Font;
+import java.awt.Image;
+
+import javax.swing.Icon;
+import javax.swing.ImageIcon;
 import javax.swing.JLabel;
 import javax.swing.JPanel;
 
 public class PanelInicio extends JPanel{
+	public PanelInicio() {
+		setLayout(null);
+		
+		JLabel lblImagen = new JLabel("");
+		lblImagen.setBounds(93, 101, 269, 199);
+		ImageIcon imgIcon = new ImageIcon("img/MissRobot.png");
+		Image imgEscalada = imgIcon.getImage().getScaledInstance(lblImagen.getWidth(),
+				lblImagen.getHeight(), Image.SCALE_SMOOTH);
+		Icon iconoEscalado = new ImageIcon(imgEscalada);
+		setLayout(null);
+		lblImagen.setIcon(iconoEscalado);
+		add(lblImagen);
+		
+		JLabel lblSaludoInicial = new JLabel("Bienvenidos a la tienda de Miss Robot");
+		lblSaludoInicial.setFont(new Font("Garamond", Font.BOLD, 19));
+		lblSaludoInicial.setBounds(68, 43, 320, 22);
+		add(lblSaludoInicial);
+	}
 
 	private JLabel lblInformacion;
 	private JLabel lblImagen;
@@ -20,6 +43,4 @@ public class PanelInicio extends JPanel{
 	public void setLblImagen(JLabel lblImagen) {
 		this.lblImagen = lblImagen;
 	}
-	
-	
 }
