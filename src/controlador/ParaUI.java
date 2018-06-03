991804f3f28db270a2c6dc39cacb3a8a56f4da4a
Macolbar <elgithubdemacolbar@gmail.com>
2018-06-04 00:26:10 +0200

    otro paraUI

--- src/controlador/ParaUI.java
+++ src/controlador/ParaUI.java
@@ -3,8 +3,10 @@ package controlador;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 
+import modelo.Cliente;
 import modelo.DatoActual;
 import modelo.Logica;
+import validacion.Validator;
 import vista.UI;
 import vista.vistaUI;
 
@@ -16,6 +18,7 @@ public class ParaUI extends UI{
 	private Consultar consultar;
 	private DarAlta darAlta;
 	private Insertar insertar;
+	Validator validador = new Validator();
 	
 	
 	public ParaUI(){
@@ -45,7 +48,7 @@ public class ParaUI extends UI{
 			@Override
 			public void actionPerformed(ActionEvent e) {
 				todosInvisibles();
-				panelDarAltaCliente.setVisible(true);
+				panelDarAltaArticulo.setVisible(true);
 				
 			}
 		});
@@ -146,6 +149,96 @@ public class ParaUI extends UI{
 				todosInvisibles();
 				panelConsultar.setVisible(true);
 				
+			}
+		});
+		
+		panelCliente.getBtnDarAlta().addActionListener(new ActionListener() {
+			
+			@Override
+			public void actionPerformed(ActionEvent arg0) {
+				/*altaCliente.setLblError("");
+				altaCliente.setVisible(true);
+				
+				altaCliente.getBtnDarDeAlta().addActionListener(new ActionListener() {
+					
+					@Override
+					public void actionPerformed(ActionEvent e) {
+						String nombre = altaCliente.getTxtNombre().getText();
+						String dni = altaCliente.getTxtDNI().getText();
+						String dire = altaCliente.getTxtDireccion().getText();
+						String tlf = altaCliente.getTxtTelf().getText();
+						altaCliente.vaciarTextos();
+						
+						if(validador.isPhone(tlf)&&!validador.isNumber(nombre)){//averiguar las validaciones
+							acciones.getLogica().crearCliente( dni, nombre, dire, tlf);
+							consulta.setComboBox(acciones.getLogica().obtenerComboBox(consulta.getComboBox(), dni));
+							altaCliente.setLblError("");
+							todosInvisibles();
+							panelCliente.setVisible(true);
+						}else{
+							altaCliente.setLblError("Ha habido un error dando de alta al cliente");
+						}
+					}
+				});*/
+				
+			}
+		});
+		
+		panelCliente.getBtnConsultarCliente().addActionListener(new ActionListener() {
+			
+			@Override
+			public void actionPerformed(ActionEvent arg0) {
+				/*consulta.getTextArea().setText("");
+				consulta.getBtnVolver().addActionListener(new ActionListener() {
+					
+					@Override
+					public void actionPerformed(ActionEvent e) {
+						todosInvisibles();
+						panelCliente.setVisible(true);
+						
+						
+					}
+				});
+				consulta.setVisible(true);
+				consulta.getComboBox().addActionListener(new ActionListener() {
+					
+					@Override
+					public void actionPerformed(ActionEvent e) {
+						consulta.setTextArea(acciones.getLogica().seleccionarCliente((Cliente) consulta.getComboBox().getSelectedItem(), consulta.getTextArea()));
+						//comboBox= listaClientes
+						//al click, poner texto de info del cliente
+						
+					}
+				});*/
+				
+			}
+		});
+		
+		panelCliente.getBtnBorrarCliente().addActionListener(new ActionListener() {
+			
+			@Override
+			public void actionPerformed(ActionEvent arg0) {
+				
+				borrarCliente.getBtnVolver().addActionListener(new ActionListener() {
+					
+					@Override
+					public void actionPerformed(ActionEvent e) {
+						todosInvisibles();
+						panelCliente.setVisible(true);
+						
+						
+					}
+				});
+				
+				borrarCliente.setVisible(true);
+				borrarCliente.getComboBox().addActionListener(new ActionListener() {
+					
+					@Override
+					public void actionPerformed(ActionEvent e) {
+						//logica.borrarCliente(borrar.getComboBox(), borrar.getComboBox().getSelectedItem().toString());
+						
+					}
+				});
 			}
 		});
 		
