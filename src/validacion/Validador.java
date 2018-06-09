6dfceaf51fb06beb72bea61bd61b2dbb9aecc0d7
Linazzy <36626979+Linazzy@users.noreply.github.com>
2018-06-09 19:55:24 +0200

    empiezo ooootra vez con los botones de articulo

---
+++ src/validacion/Validador.java
@@ -0,0 +1,27 @@
+package validacion;
+
+public class Validador {
+
+	public boolean validarArticulo(String nombre, String proveedor, String descripcion, int cantidad){
+		if(validarString(nombre) && validarString(proveedor) && validarString(descripcion) && validarInt(cantidad)){
+			return true;
+		}
+		return false;
+	}
+	
+	private boolean validarString(String nombre){
+		if(nombre==null)
+			return false;
+		if(nombre.equals(""))
+			return false;
+		if(nombre.length()<3)
+			return false;
+		return true;
+	}
+	
+	private boolean validarInt(int cantidad){
+		if(cantidad<=0)
+			return false;
+		return true;
+	}
+}
