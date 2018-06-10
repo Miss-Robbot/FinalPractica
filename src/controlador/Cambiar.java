ff35116829f2ed5346aad11a0f7b131c0b43ec61
Linazzy <36626979+Linazzy@users.noreply.github.com>
2018-06-10 14:44:11 +0200

    intentando coger cosas yoly

--- src/controlador/Cambiar.java
+++ src/controlador/Cambiar.java
@@ -1,13 +1,26 @@
 package controlador;
 
+import java.util.ArrayList;
+
+import javax.swing.JComboBox;
+
 import modelo.Articulo;
 import modelo.Precio;
 
 public class Cambiar extends Acciones{
 
-	//Se puede modificar tanto la entrada de datos como la salida
-	
-	public void cambiarPrecioArticulo(Articulo articulo,Precio precioNuevo){
-		//getLogica().cambiarPrecio(precioNuevo, articulo);
+	/**
+	 * Llena el combobox de artículos
+	 * 
+	 * @param combobox
+	 * @param articulos
+	 */
+	public void rellenarComboboxArticulo(JComboBox combobox) {
+		combobox.removeAllItems();
+		ArrayList<Articulo> articulos= getLogica().getListaArticulos();
+		for (Articulo articulo : articulos) {
+			combobox.addItem(articulo.toString());
+		}
 	}
+	
 }
